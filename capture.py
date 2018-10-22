f1e52800585c4f5ee36d0e05c229d50317ae7e1f
Bruce <mcmoranbjr@gmail.com>
2018-10-22 09:10:14 -0700

    Zoom control
    Allow a keyboard operator to control the zoom level of the camera.
    
    Next it'd be great to do better feature detection and to auto-focus based on orb size. If orbs are larger, zoom until they get smaller. Track the maximum size stop zooming when a minima is reached. Obviously zoom in/out if the minima is passed.

--- capture.py
+++ capture.py
@@ -8,14 +8,38 @@ import sys
 import gphoto2 as gp
 import numpy as np
 import cv2
-import time
-from PIL import Image
+def zoom(key, camera):
+    # Focus on a point further away
+    if key == ord('k'):
+        print("Zooming in")
+        w = gp.check_result(gp.gp_widget_new(gp.GP_WIDGET_TEXT, "focus"))
+        gp.gp_widget_set_name(w, "manualfocusdrive")
+        gp.gp_widget_set_value(w, "Far 3")
+        gp.gp_camera_set_single_config(camera, "manualfocusdrive", w)
+    # Focus on a nearer point
+    if key == ord('j'):
+        print("Zooming out")
+        w = gp.check_result(gp.gp_widget_new(gp.GP_WIDGET_TEXT, "focus"))
+        gp.gp_widget_set_name(w, "manualfocusdrive")
+        gp.gp_widget_set_value(w, "Near 3")
+        gp.gp_camera_set_single_config(camera, "manualfocusdrive", w)
 
 camera = gp.check_result(gp.gp_camera_new())
 gp.check_result(gp.gp_camera_init(camera))
 orb = cv2.ORB_create()
+#config = camera.list_config()
+config = gp.check_result(gp.gp_camera_list_config(camera))
+# set_single_config, set_config, get_config
+#for e in config:
+#    print("Config element", e)
+def feature_index(config, feature):
+    i = -1
+    for f in config:
+        if f[0] == feature:
+          return i
+        i+=1
+focus = feature_index(config, "manualfocusdrive")
 while (True):
-    print("Capturing preview")
     # print(dir(gp))
     # camera.set_config
     preview = gp.check_result(gp.gp_camera_capture_preview(camera))
@@ -27,10 +51,7 @@ while (True):
     frame = cv2.drawKeypoints(dec, kp, None, color=(0,0,255), flags=0)
     cv2.imshow('frame', frame)
     #cv2.imshow('image', dec)
-    # Works to show image
-    #i = Image.open(io.BytesIO(jpg))
-    #i.show()
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    key = cv2.waitKey(25) # 25 ms wait for a keypress
+    if key == ord('q'):
         break
-	
-	
+    zoom(key, camera)
