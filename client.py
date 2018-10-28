e40edce32fbb7c69dc765b4b776896ff5bc6e549
Bruce <mcmoranbjr@gmail.com>
2018-10-27 18:14:47 -0700

    Server and Client with options
    * server.py audofile.aac
    * client.py server-ip

--- client.py
+++ client.py
@@ -5,19 +5,10 @@ import socket
 import struct
 import time
 import picamera
+import sys
 
-client_socket = socket.socket()
-client_socket.connect(("10.0.0.27", 8000))
-
-connection = client_socket.makefile('wb')
-
-try:
-    camera = picamera.PiCamera()
-    camera.resolution = (640, 480)
-    camera.start_preview()
-    time.sleep(2)
-
-    stream =io.BytesIO()
+def stream_photos(camera, connection):
+    stream = io.BytesIO()
     for preview in camera.capture_continuous(stream, "jpeg"):
         connection.write(struct.pack("<L", stream.tell()))
         connection.flush()
@@ -27,9 +18,28 @@ try:
         # Reest for next capture
         stream.seek(0)
         stream.truncate()
-    # Send the done signal
     connection.write(struct.pack("<L", 0))
-finally:
-    connection.close()
-    client_socket.close()
+    # Send the done signal
+def run(srv_addr):
+    camera = picamera.PiCamera()
+    camera.resolution = (640, 480)
+    camera.start_preview()
+    client_socket = socket.socket()
+    client_socket.connect((srv_addr, 8000))
+    connection = client_socket.makefile('wb')
+    try:
+        stream_photos(camera, connection)
+    except BrokenPipeError as e:
+        print("Server shutdown. Exiting...")
+    finally:
+        connection.close()
+        client_socket.close()
 
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        print("Need a server address")
+        sys.exit(1)
+    try:
+        run(sys.argv[1])
+    except ConnectionRefusedError as e:
+        print("Server is down")
--- server.py
+++ server.py
@@ -4,17 +4,41 @@ import cv2
 import numpy as np
 import socket
 import struct
+import subprocess
+import sys
+import time
 from PIL import Image
+orb = cv2.ORB_create()
+n = 0
+lock = time.time() -1
+def isstable(orb, image):
+    kp = orb.detect(image, None)
+    kp, des = orb.compute(image, kp)
+    # > 45: not blurry
+    # > 80: sharp
+    blurriness = cv2.Laplacian(image, cv2.CV_64F).var()
+    # > 400: Sharp, > 250 : Acceptable
+    # blurriness > 110 in highlight
+    print("Blurriness", blurriness, "Focus ", len(kp),"/500")
+    if blurriness > 100 and len(kp) > 450:
+        print("    Sharp", blurriness, "Focus ", len(kp),"/500")
+        return True
+    return False
+def run_scary(audio):
+    print("run_scary")
+    # Call music player
+    #subprocess.run(["afplay", audio])
+
 server_socket = socket.socket()
-server_socket.bind(("10.0.0.27", 8000))
+server_socket.bind(("10.0.0.16", 8000))
 server_socket.listen(0)
 connection = server_socket.accept()[0].makefile('rb')
+audio = sys.argv[1]
 try:
     while True:
         # Read the image length
         l = struct.unpack("<L", connection.read(struct.calcsize("<L")))[0]
         image_stream = io.BytesIO()
-        print("Reading ", l, "bytes of image")
         image_stream.write(connection.read(l))
         # Rewind the stream
         image_stream.seek(0)
@@ -25,6 +49,10 @@ try:
         # PIL is RGB, CV2 is BGR Color
         image = cv2.cvtColor(np.array(preview), cv2.COLOR_RGB2BGR)
         cv2.imshow("pi image", image)
+        if isstable(orb, image) and lock < time.time():
+            lock = time.time()+10
+            run_scary(audio)
+            n += 1
         cv2.waitKey(10) #Wait 10ms for a keypress
 finally:
     connection.close()
