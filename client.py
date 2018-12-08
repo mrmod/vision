fde35c83374905a7b86f964f29bbdb0f2f745ea2
bruce <pi@2gbcomputing.net>
2018-12-08 02:00:30 +0000

    Camera client
    A PiCamera based client.
    
    A `nightview.json` can configure the following keys with values:
    ```
    {
      brightness: 60,
      contrast: -25,
      framerate: 40,
      awbmode: cloudy
    }
    ```
    
    A server should be given to avoid using the default

--- client.py
+++ client.py
@@ -6,29 +6,75 @@ import struct
 import time
 import picamera
 import sys
+import argparse
+import datetime
+import json
 
-def stream_photos(camera, connection):
-    stream = io.BytesIO()
-    for preview in camera.capture_continuous(stream, "jpeg"):
-        connection.write(struct.pack("<L", stream.tell()))
-        connection.flush()
-        # Rewind and send the image
-        stream.seek(0)
-        connection.write(stream.read())
-        # Reest for next capture
-        stream.seek(0)
-        stream.truncate()
-    connection.write(struct.pack("<L", 0))
-    # Send the done signal
-def run(srv_addr):
-    camera = picamera.PiCamera()
+class PhotoStreamer(object):
+    def __init__(self, camera, connection, args):
+        self._stream = io.BytesIO()
+        self.camera = camera
+        self.connection = connection
+        self.args = args
+        self.config = args.__dict__
+
+        self.camera.start_preview()
+
+    def stream(self): 
+        counter = 0
+        for preview in self.camera.capture_continuous(self._stream, "jpeg"):
+            if counter % 10 == 0:
+                conter = 0
+                self.log(self.camera)
+                self.read_settings_file()
+                self.update_settings(self.camera)
+            counter += 1
+            # Write the size of the bytearray
+            image_size = self._stream.tell()
+            #connection.write(struct.pack("<L", image_size))
+            self.connection.write(struct.pack("<L", image_size))
+            self.connection.flush()
+            # Rewind and send the image
+            now_at = self._stream.seek(0)
+            self.connection.write(self._stream.read())
+            # Reset for next capture
+            self._stream.seek(0)
+            self._stream.truncate()
+        # Send the done signal
+        self.connection.close()
+    def log(self, camera):
+            with open("awbGains", "a+") as f:
+                f.write("{} : {}\n".format(datetime.datetime.now(), camera.awb_gains))
+            print("{},{},{}".format(datetime.datetime.now(), camera.awb_gains, camera.brightness))
+    
+    def update_settings(self, camera):
+        camera.brightness = args.brightness
+        camera.awb_mode = args.awbmode
+        camera.contrast = args.contrast
+        camera.brightness = args.brightness
+    
+    def read_settings_file(self ):
+        new_config = {}
+        try:
+            with open("nightview.json") as f:
+                new_config = json.load(f)
+        except json.JSONDecodeError:
+            print("{}: error parsing nightview.json".format(datetime.datetime.now()))
+
+        for key in new_config:
+            self.config[key] = new_config[key]
+        for key in self.config:
+            method = setattr(self.args, key, self.config[key])
+
+    
+def run(args):
+    camera = picamera.PiCamera(framerate=args.framerate)
     camera.resolution = (640, 480)
-    camera.start_preview()
     client_socket = socket.socket()
-    client_socket.connect((srv_addr, 8000))
+    client_socket.connect((args.server, 8000))
     connection = client_socket.makefile('wb')
     try:
-        stream_photos(camera, connection)
+        PhotoStreamer(camera, connection, args).stream()
     except BrokenPipeError as e:
         print("Server shutdown. Exiting...")
     finally:
@@ -36,10 +82,14 @@ def run(srv_addr):
         client_socket.close()
 
 if __name__ == "__main__":
-    if len(sys.argv) <= 1:
-        print("Need a server address")
-        sys.exit(1)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--brightness", type=int, default=50)
+    parser.add_argument("--framerate", type=int, default=40)
+    parser.add_argument("--server", type=str, default="10.0.0.100")
+    parser.add_argument("--awbmode", type=str, default="auto")
+    parser.add_argument("--contrast", type=int, default=0, help="-100 0 100")
+    args = parser.parse_args()
     try:
-        run(sys.argv[1])
+        run(args)
     except ConnectionRefusedError as e:
         print("Server is down")
